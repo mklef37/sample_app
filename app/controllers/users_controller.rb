255bcb9f9d7cdac8549a9e8dee2e767d850aebd5
Max Klefstad <mk672@cornell.edu>
2012-05-23 14:17:02 -0400

    Finish user edit, update, index, destwoy - ch. 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+before_filter :signed_in_user, only: [:index, :edit, :update]
+before_filter :correct_user, only: [:edit, :update]
+before_filter :admin_user, only: :destroy
+
   def new
   	@user = User.new
   end
 
   def show
-  	@user = User.find(params[:id])
+    if(User.exists?(params[:id]))
+  	  @user = User.find(params[:id])
+    else
+      redirect_to root_path
+      flash[:error] = "The user with given ID does not exist"
+    end
   end
 
   def create
@@ -17,5 +26,46 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile Updated! Woot!!!!1!!One!"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Baboom, it's gone"
+    redirect_to users_path
+  end
   
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in." unless signed_in?
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_path unless current_user.admin?
+    end
 end
